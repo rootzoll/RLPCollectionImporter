63099a22700180a81416e99a4b76d345064e9ae5
rootzoll <christian@geektank.de>
2016-07-23 18:23:32 +0200

    Hello World Commit

---
+++ src/net/edusharing/collections/rlpcollection/RLPCollectionImporter.java
@@ -0,0 +1,11 @@
+package net.edusharing.collections.rlpcollection;
+
+public class RLPCollectionImporter {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		
+	}
+
+}
